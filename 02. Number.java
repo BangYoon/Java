d177b096558522a81e0be5fdc752c153b1fcfef1
BangYoon <yoonb720@naver.com>
2019-02-01 16:27:10 +0900

    Create 02. Number.java

---
+++ 02. Number.java
@@ -0,0 +1,13 @@
+package bangyoon.java.eclipse;
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1+2);
+		System.out.println(1.2+1.3);
+		System.out.println(2*5);
+		System.out.println(6/1);
+	}
+
+} 
