eb520197446017fb351733b2d394498a3e816ae3
BangYoon <yoonb720@naver.com>
2019-02-01 17:13:10 +0900

    Create 04. Variable.java

---
+++ 04. Variable.java
@@ -0,0 +1,30 @@
+package bangyoon.java.eclipse;
+
+public class Variable {
+
+	public static void main(java.lang.String[] args) {
+
+		int a; //정수
+		a = 1;
+		System.out.println(a + 1); // 2
+		a = 2;
+		System.out.println(a + 1); // 3
+
+		
+		double aa; //실수
+		aa = 1.1;
+		System.out.println(aa + 1.1); // 2.2
+		aa = 2.1;
+		System.out.println(aa + 1.1); // 3.2
+		
+		
+		java.lang.String first; //문자열
+		first = "coding";
+		System.out.println(first + " " + "everybody");
+
+		/*
+		 * 이거슨 긴줄 주석!!
+		 */
+	}
+
+}
