7302ba299603f161b43f9527b946ce84576f684f
BangYoon <yoonb720@naver.com>
2019-02-01 16:27:32 +0900

    Create 03. String.java

---
+++ 03. String.java
@@ -0,0 +1,12 @@
+package bangyoon.java.eclipse;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println('문'); //문자는 '' or ""
+		System.out.println("문"); 
+		System.out.println("문자열"); //문자열은 ""
+	}
+
+}
