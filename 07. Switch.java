536749ff402660dc71d7b13b01ac1c7465f11e1e
BangYoon <yoonb720@naver.com>
2019-02-03 23:28:10 +0900

    Create 07. Switch.java

---
+++ 07. Switch.java
@@ -0,0 +1,38 @@
+package bangyoon.java.eclipse;
+
+public class Switch {
+
+	public static void main(java.lang.String[] args) {
+		System.out.println("switch(1)");
+        switch(1){
+        case 1:
+            System.out.println("one");
+        case 2:
+            System.out.println("two");
+        case 3:
+            System.out.println("three");
+        }
+         
+        System.out.println("switch(2)");
+        switch(2){
+        case 1:
+            System.out.println("one");
+        case 2:
+            System.out.println("two");
+        case 3:
+            System.out.println("three");
+        }
+         
+        System.out.println("switch(3)");
+        switch(3){
+        case 1:
+            System.out.println("one");
+        case 2:
+            System.out.println("two");
+        case 3:
+            System.out.println("three");
+        }
+
+	}
+
+}
